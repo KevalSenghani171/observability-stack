ec06554347bf071724fe266e67b410249006ae38
KevalSenghani171 <senghanikeval@gmail.com>
2026-02-28 14:00:53 +0530

    fixed terraform tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
-
+# Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
+# Get subnets inside default VPC
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
@@ -10,34 +11,35 @@ data "aws_subnets" "default" {
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.default.id
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.default.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "rta" {
-  subnet_id      = aws_subnet.default.id
-  route_table_id = aws_route_table.rt.id
-}
-
+# Security Group
 resource "aws_security_group" "kubernetes_sg" {
-  vpc_id = aws_vpc.default.id
   name   = "kubernetes-sg"
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "Kubernetes API"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Node to Node"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -46,18 +48,25 @@ resource "aws_security_group" "kubernetes_sg" {
   }
 }
 
+# Existing Instance Profile
 data "aws_iam_instance_profile" "existing" {
   name = "devops-aws"
 }
 
+# EC2 Instances
 resource "aws_instance" "kubernetes_nodes" {
   count         = 3
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.default.id
+
+  # Use first subnet from default VPC
+  subnet_id = data.aws_subnets.default.ids[0]
+
   vpc_security_group_ids = [aws_security_group.kubernetes_sg.id]
-  key_name      = var.key_name
-  iam_instance_profile = data.aws_iam_instance_profile.existing.name
+  key_name               = var.key_name
+  iam_instance_profile   = data.aws_iam_instance_profile.existing.name
+
+  associate_public_ip_address = true
 
   tags = {
     Name = "kubernetes-node-${count.index + 1}"
