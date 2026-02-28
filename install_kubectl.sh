af3340ac7dbf2c8fb5c320dc4c5002621ff7082f
KevalSenghani171 <senghanikeval@gmail.com>
2026-02-28 15:33:14 +0530

    added scripts

---
+++ install_kubectl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | \
+sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] \
+https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /" | \
+sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt install -y kubectl
