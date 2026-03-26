0e6c4ca8ae68ab5de529d9072f548e5cbd8c8763
KevalSenghani171 <senghanikeval@gmail.com>
2026-03-26 13:57:42 +0530

    updated grafana

--- Scripts/grafana_deploy.sh
+++ Scripts/grafana_deploy.sh
@@ -21,23 +21,37 @@ echo -e "\n"
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
+envssubst < grafana-resources/datasources/datasource.yaml.tpl >> grafana-resources/datasources/datasource.yaml
+rm grafana-resources/datasources/datasource.yaml.tpl
+
 echo -e "\n"
 cd grafana 
 ls -l
 echo ".....Grafana......"
 kubectl create secret generic grafana-mysql -n devops-tools --from-literal=password="$GRAFANA_DB_PASSWORD" --dry-run=client -o yaml | kubectl apply -f -
 
-helm upgrade --install grafana grafana/grafana -f values.yaml  -n devops-tools    
+helm upgrade --install grafana grafana/grafana -f values.yaml -f override/values-prod.yaml  -n devops-tools    
 
 kubectl get all -n  devops-tools 
 
 sleep 45
 echo -e "\n"
 echo ".....Copy dashboards......"
-# grafana_pod=`kubectl -n devops-tools get po -l app.kubernetes.io/instance=grafana -o=jsonpath='{.items[0].metadata.name}'`
-# kubectl -n devops-tools exec -it ${grafana_pod} -c grafana -- rm -rf /var/lib/grafana/dashboards/OBS
-# kubectl -n devops-tools cp dashboards/OBS ${grafana_pod}:/var/lib/grafana/dashboards/ -c grafana
-# curl -uadmin:Admin@123 -H -s -X POST "http://localhost:3000/api/admin/provisioning/dashboards/reload"
+grafana_pod=`kubectl -n devops-tools get po -l app.kubernetes.io/instance=grafana -o=jsonpath='{.items[0].metadata.name}'`
+
+
+kubectl -n devops-tools exec -it ${grafana_pod} -c grafana -- rm -rf /var/lib/grafana/dashboards/OBS
+kubectl -n devops-tools cp grafana-resources/dashboards/OBS ${grafana_pod}:/var/lib/grafana/dashboards/ -c grafana
+kubectl -n devops-tools exec -it ${grafana_pod} -c grafana -- curl -u admin:Admin@123 -H -s -X POST "http://localhost:3000/api/admin/provisioning/dashboards/reload"
+
+
+kubectl -n devops-tools exec -it ${grafana_pod} -c grafana -- rm -rf /etc/grafana/provisioning/datasources 
+kubectl -n devops-tools cp grafana-resources/datasources ${grafana_pod}:/etc/grafana/provisioning -c grafana
+kubectl -n devops-tools exec -it ${grafana_pod} -c grafana -- curl -u admin:Admin@123 -H -s -X POST "http://localhost:3000/api/admin/provisioning/datasources/reload"
+
+kubectl -n devops-tools exec -it ${grafana_pod} -c grafana -- rm -rf /etc/grafana/provisioning/alerting
+kubectl -n devops-tools cp grafana-resources/alerting ${grafana_pod}:/etc/grafana/provisioning -c grafana
+kubectl -n devops-tools exec -it ${grafana_pod} -c grafana -- curl -u admin:Admin@123 -H -s -X POST "http://localhost:3000/api/admin/provisioning/alerting/reload"
 
 
 
