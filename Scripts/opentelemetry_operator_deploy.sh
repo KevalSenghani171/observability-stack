2437e6e7f6d659b9175ed88a90393c8f136ca1f6
KevalSenghani171 <senghanikeval@gmail.com>
2026-03-28 18:13:45 +0530

    added values file

--- Scripts/opentelemetry_operator_deploy.sh
+++ Scripts/opentelemetry_operator_deploy.sh
@@ -22,7 +22,7 @@ echo -e "\n"
 cd opentelemetry-operator
 ls -l
 
-helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator -n observability \
+helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator -f values.yaml -f overrides/values.yaml -n observability \
 --set "manager.collectorImage.repository=ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-k8s" \
 --set admissionWebhooks.certManager.enabled=false \
 --set admissionWebhooks.autoGenerateCert.enabled=true
