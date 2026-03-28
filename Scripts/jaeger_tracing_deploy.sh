5083e11b0609ee3b0fd3ad7be4d90b528d53f3c7
KevalSenghani171 <senghanikeval@gmail.com>
2026-03-28 15:08:29 +0530

    added jaeger script

---
+++ Scripts/jaeger_tracing_deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo -e "\n"
+
+
+set -e
+
+echo "Current working directory:"
+pwd
+
+echo "Listing files:"
+ls -l
+echo -e "\n"
+
+echo "Using latest values.yaml from Git"
+
+echo -e "\n"
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo update
+
+echo -e "\n"
+cd opentelemetry-operator
+ls -l
+
+helm upgrade --install jaeger jaegertracing/jaeger -n observability -f values.yaml
+    
+
+kubectl get all -n  observability 
+
+sleep 45
+echo -e "\n"
+
+
+if [ $? -eq 0 ]; then
+   echo "Job Ended Succesfully"
+else
+   exit
+fi
