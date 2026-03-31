f563bedac93c3eb77ed90c45cc8e37ac325afcd4
KevalSenghani171 <senghanikeval@gmail.com>
2026-03-31 11:43:05 +0530

    updated repo

---
+++ Scripts/gocd_deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo -e "\n"
+
+set -e
+
+echo "Current working directory:"
+pwd
+
+echo "Listing files:"
+ls -l
+echo -e "\n"
+
+
+
+echo -e "\n"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+echo -e "\n"
+cd gocd 
+ls -l
+
+helm upgrade --install gocd gocd/gocd -n gocd -f values.yaml  -n gocd    
+
+kubectl get all -n  gocd 
+
+
+sleep 45
+echo -e "\n"
+
+
+if [ $? -eq 0 ]; then
+   echo "Job Ended Succesfully"
+else
+   exit
+fi
