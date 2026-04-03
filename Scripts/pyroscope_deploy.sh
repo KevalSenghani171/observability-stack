95fee0e3c4617eda45b2d5e1a582171291b41b22
KevalSenghani171 <senghanikeval@gmail.com>
2026-04-03 10:06:45 +0530

    added alloy and pyroscope

---
+++ Scripts/pyroscope_deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo -e "\n"
+
+
+set -e
+
+echo "Current working directory:"
+pwd
+
+echo "Listing files:"
+ls -l
+echo -e "\n"
+
+echo "Using latest values.yaml from Git"
+
+echo -e "\n"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+echo -e "\n"
+cd pyroscope 
+ls -l
+
+helm upgrade --install tempo grafana/pyroscope -f values.yaml -f override/values.yaml -n devops-tools    
+
+kubectl get all -n  devops-tools 
+
+sleep 45
+echo -e "\n"
+
+
+if [ $? -eq 0 ]; then
+   echo "Job Ended Succesfully"
+else
+   exit
+fi
